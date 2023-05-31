4674ec4360a3427f3eb0c764079ce36ea1296281
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2023-05-31 11:05:46 +0500

    change urls

--- books/urls.py
+++ books/urls.py
@@ -3,8 +3,8 @@ from .views import BookListApiView, BookCreateApiView,\
     BookDeleteApiView, BookUpdateApiView
 
 urlpatterns = [
-    path('books/', BookListApiView.as_view()),
-    path('books/create/', BookCreateApiView.as_view()),
-    path('books/delete/<int:pk>/', BookDeleteApiView.as_view()),
-    path('books/update/<int:pk>/', BookUpdateApiView.as_view()),
+    path('books', BookListApiView.as_view()),
+    path('books/create', BookCreateApiView.as_view()),
+    path('books/delete/<int:pk>', BookDeleteApiView.as_view()),
+    path('books/update/<int:pk>', BookUpdateApiView.as_view()),
 ]
