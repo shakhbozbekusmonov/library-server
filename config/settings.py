a92a37edd77ec0faa35cd655104491273c3e45f3
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2023-05-31 11:15:53 +0500

    CHange cors

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ CORS_ALLOW_METHODS = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:5173',)
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:5173', 'https://web-production-98f5.up.railway.app')
 
 TEMPLATES = [
     {
